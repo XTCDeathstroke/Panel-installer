00316783682310a9906cbafee2f6952ff46e9aca
XTCDeathstroke <53090946+XTCDeathstroke@users.noreply.github.com>
2024-02-25 22:26:43 -0600

    Update config_PHP.sh

--- config_PHP.sh
+++ config_PHP.sh
@@ -3,8 +3,14 @@
 # Enable error handling - script will exit immediately if any command returns a non-zero exit status
 set -e
 
+# Update package lists
+sudo apt update
+
+# Install PHP 8.0 and extensions
+sudo apt install -y php8.0 php8.0-cli php8.0-gd php8.0-mysql php8.0-pdo php8.0-mbstring php8.0-tokenizer php8.0-bcmath php8.0-xml php8.0-fpm php8.0-curl php8.0-zip
+
 # Enable PHP-FPM
-if systemctl enable php8.0-fpm; then
+if sudo systemctl enable php8.0-fpm; then
     echo "PHP-FPM enabled."
 else
     echo "Failed to enable PHP-FPM."
@@ -12,7 +18,7 @@ else
 fi
 
 # Start PHP-FPM
-if systemctl start php8.0-fpm; then
+if sudo systemctl start php8.0-fpm; then
     echo "PHP-FPM started."
 else
     echo "Failed to start PHP-FPM."
